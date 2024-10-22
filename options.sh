9b961b82806cf41d920fd898e08f9f72390913e8
frederico-klein <frekle@kth.se>
2024-10-22 17:25:56 +0200

    log shows correctly now

--- options.sh
+++ options.sh
@@ -37,17 +37,17 @@ USE_REALSENSE=true
 
 log_info()
 {
-	echo -e "\e[97m [INFO] [$EPOCHREALTIME]: $1 \e[0m"
+	echo -e "\e[97m [INFO] [$EPOCHREALTIME]: $@ \e[0m"
 
 }
 log_warn()
 {
-	echo -e "\e[33m [WARN] [$EPOCHREALTIME]: $1 \e[0m"
+	echo -e "\e[33m [WARN] [$EPOCHREALTIME]: $@ \e[0m"
 
 }
 log_debug()
 {
-	echo -e "\e[32m [DEBUG] [$EPOCHREALTIME]: $1 \e[0m"
+	echo -e "\e[32m [DEBUG] [$EPOCHREALTIME]: $@ \e[0m"
 
 }
 
